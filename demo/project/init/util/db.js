4e8554196893a71d53c8f07a62a61ff179b79264
tobegreatman <41740663+tobegreatman@users.noreply.github.com>
2019-10-18 17:04:01 +0800

    Update db.js
    修复config.js中如果port不是3306，导致无法登录数据库，项目启动异常

--- demo/project/init/util/db.js
+++ demo/project/init/util/db.js
@@ -6,7 +6,8 @@ const pool = mysql.createPool({
   host     :  dbConfig.HOST,
   user     :  dbConfig.USERNAME,
   password :  dbConfig.PASSWORD,
-  database :  dbConfig.DATABASE
+  database :  dbConfig.DATABASE,
+  port     :  dbConfig.PORT// config中如果port不是3306，会导致登录被拒，修复port无法映射过来
 })
 
 
